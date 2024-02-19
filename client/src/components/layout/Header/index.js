13a25394f36af91ec852000c63de73f32e383e80
thesajalghosh <sajalghosh.itrd@gmail.com>
2024-02-19 12:13:34 +0530

    changes in header for login issue

--- client/src/components/layout/Header/index.js
+++ client/src/components/layout/Header/index.js
@@ -19,6 +19,7 @@ const Header = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [iscategoryOpen, setiscategoryOpen] = useState(false);
   const token = localStorage.getItem("token");
+  console.log(token);
 
   const navigate = useNavigate();
   const handleLogout = () => {
